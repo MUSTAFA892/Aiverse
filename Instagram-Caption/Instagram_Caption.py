5269731cda40833efb62cb3e1e0095aa48183367
MUSTAFA892 <mustafatinwala6@gmail.com>
2025-08-13 14:06:30 +0530

    updated the instagram file

--- Instagram-Caption/Instagram_Caption.py
+++ Instagram-Caption/Instagram_Caption.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 # Configure CORS to allow requests from frontend
 CORS(app, supports_credentials=True, resources={
     r"/api/*": {
-        "origins": ["http://localhost:3000"],
+        "origins": ["http://localhost:3000","https://aiverse-icip.onrender.com"],
         "methods": ["GET", "POST", "OPTIONS"],
         "allow_headers": ["Content-Type", "Authorization"],
         "expose_headers": ["Content-Type"],
